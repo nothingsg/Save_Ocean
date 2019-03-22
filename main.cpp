89dac8ccfaccd2748074e8d2ea068b82d953a50e
郭志武 <2605946471@qq.com>
2019-03-22 13:49:39 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <graphics.h>
+#include <iostream>
+
+int main()
+{
+	initgraph(640, 480);   // 创建绘图窗口，大小为 640x480 像素
+	circle(200, 200, 100); // 画圆，圆心(200, 200)，半径 100
+	getchar();
+	return 0;
+}

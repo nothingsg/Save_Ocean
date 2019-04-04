d3f3c9145b63a08e8ea1d23aaf847e48832271b0
睿 张 <576146393@qq.com>
2019-04-04 21:21:31 +0800

    改为相对路径

--- Gmae.cpp
+++ Gmae.cpp
@@ -29,7 +29,7 @@ void Game::oc_GameInit()
 
 void Game::oc_GameLoad()
 {
-	loadimage(&test_img, L"D:\\工程\\VS\\工作室游戏\\Save_Ocean\\资源文件\\测试图片.png", oc_cxGame, oc_cyGame, false);
+	loadimage(&test_img, L".\\资源文件\\测试图片.png", oc_cxGame, oc_cyGame, false);
 	setbkmode(TRANSPARENT);	//设置文字输出是背景颜色为透明
 }
 

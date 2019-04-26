3a539e6c1b5de53443078086030c6022aac73de3
123 陈 <2210990302@qq.com>
2019-04-26 23:51:07 +0800

    按钮可以实时变化

--- main.cpp
+++ main.cpp
@@ -9,10 +9,18 @@ int main()
 	{
 		IMAGE ready;
 		HWND oc_hWnd = GetHWnd();
+		IMAGE an;
+		POINT pt;
 		loadimage(&ready, L".\\资源文件\\开始界面.png", 1300, 900, false);
-		putimage(50, 0, &ready);
 		//bar(462, 353, 870, 455);//开始按钮位置
-		void FlushMouseMsgBuffer();//清空鼠标消息缓冲区
+		GetCursorPos(&pt);//检取光标的位置，以屏幕坐标表示
+		ScreenToClient(oc_hWnd, &pt);//该函数把屏幕上指定点的屏幕坐标转换成用户坐标
+		if (pt.x > 462 && pt.x < 870 && pt.y>353 && pt.y < 455)
+		{
+			loadimage(&an, L".\\资源文件\\开始界面2.png", 408, 102, false);
+		}
+		putimage(50, 0, &ready);
+		putimage(462, 353, &an);
 		static bool last = 0;
 		bool now = is_key_down(VK_LBUTTON);
 		if (!last&&now)//判断是鼠标左键是否按下

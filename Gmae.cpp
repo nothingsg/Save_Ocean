9beb47774fc9665c4828a07f409db2974497d42c
123 陈 <2210990302@qq.com>
2019-05-09 13:01:00 +0800

    新加图片到图鉴后的（图鉴一直亮直到打开图鉴后图鉴变黑），点击图鉴会直接跳到新加的那一页

--- Gmae.cpp
+++ Gmae.cpp
@@ -453,17 +453,22 @@ int flag = 0;//是否进入图鉴
 window win(300,100,700, 500);//生成图鉴窗口
 const int N = 4;//图鉴内图片数量
 int book[N+1];//标记鱼
+int new_i;//新加的鱼的序号
 int flaglight = 0;//表示图鉴不亮
 void Game::add(int num, bool light)
 {
 	book[num+1] = 1;
+	new_i = num+1;
 	if (light)
 	{
 		flaglight = 1;
 	}
 }
+
 void Game::oc_UI_Upedate()
 {
+	//book[2] = 1;
+	//new_i = 2;//测试
 	wchar_t out_text[50];
 	static int out_i = 0;
 	book[0] = 1;//封面一直存在
@@ -476,14 +481,21 @@ void Game::oc_UI_Upedate()
 		if (flag == 0 && mou.x > 100 && mou.x < 170 && mou.y>30 && mou.y < 90)//鼠标左键点击在按钮范围内
 		{
 			flag = 1;//进入图鉴
-			flaglight = 0;//图鉴变黑
-			loadimage(&win.img_i, L".\\资源文件\\图鉴\\0.jpg", win.new_x, win.new_y, false);
+			if (flaglight)
+			{
+				out_i=new_i;
+				swprintf(out_text, 50, L".\\资源文件\\图鉴\\%d.jpg", out_i);
+				loadimage(&win.img_i, out_text, win.new_x, win.new_y, false);
+			}
+			else
+				loadimage(&win.img_i, L".\\资源文件\\图鉴\\0.jpg", win.new_x, win.new_y, false);
 		}
 		else if (flag==1&&mou.x > 970 && mou.x < 1000 && mou.y>100 && mou.y < 130)//退出
 		{
 			out_i = 0;
 			flag = 0;
 			loadimage(&oc_window, L".\\资源文件\\测试图片.png", 500, 500, false);
+			flaglight = 0;//图鉴变黑
 		}
 		else if (flag==1&&mou.x > 940 && mou.x < 990 && mou.y>360 && mou.y < 390 && out_i < N)//下页
 		{
@@ -527,7 +539,10 @@ void Game::oc_UI_Draw()
 		//bar(100,30,155,120);
 		if (flaglight)
 		{
-			loadimage(&img_0, L".\\资源文件\\按钮\\图鉴3.jpg", 70, 60, false);
+			if(pt.x > 100 && pt.x < 170 && pt.y>30 && pt.y < 90)
+				loadimage(&img_0, L".\\资源文件\\按钮\\图鉴4.jpg", 70, 60, false);
+			else
+				loadimage(&img_0, L".\\资源文件\\按钮\\图鉴3.jpg", 70, 60, false);
 		}
 		else if (pt.x > 100 && pt.x < 170 && pt.y>30 && pt.y < 90)
 		{
